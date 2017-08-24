507ad6683d3d3ca893115b96d844e4bfa6b38980
Oskar Ulstein <oulstein@gmail.com>
2017-08-24 18:30:34 +0200

    test

---
+++ src/main/java/Server.java
@@ -0,0 +1,191 @@
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+
+
+public class Server {
+
+    private ServerSocket serverSocket;
+    private DataOutputStream os;
+    private ObjectInputStream input;
+    private ObjectOutputStream output;
+    private ObjectOutputStream outputA;
+    private ObjectOutputStream outputB;
+    private HashMap<String, DataOutputStream> clients;
+    private int x = 0;
+    private GpioPinDigitalOutput led1;
+    private GpioPinDigitalOutput PC;
+    final GpioController gpio = GpioFactory.getInstance();
+
+    public Server() {
+        led1 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, PinState.LOW);
+        PC = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, PinState.LOW);
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.runServer();
+    }
+
+
+    public void runServer() {
+        String in;
+        Socket socket;
+        ServerSocket serverSocket;
+        try {
+            serverSocket = new ServerSocket(12345, 100);
+            System.out.println("Server is running");
+            clients = new HashMap<String, DataOutputStream>();
+            while (true) {
+                try {
+                    socket = serverSocket.accept();
+                    os = new DataOutputStream(socket.getOutputStream());
+                    input = new ObjectInputStream(socket.getInputStream());
+                    in = (String) input.readObject();
+                    System.out.println(in);
+                    switch (in) {
+                        case "Pc client":
+                            clients.put("Pc client", os);
+                            x = 0;
+                            System.out.println(clients.get("Pc client"));
+                            break;
+                        case "Android client":
+                            clients.put("Android client", os);
+                            System.out.println(clients.get("Android client"));
+                            break;
+                    }
+
+                    System.out.println(clients);
+                    new Controller(socket).start();
+                    System.out.println("hello client " + socket.getRemoteSocketAddress());
+                }catch (IOException e) {
+                    System.out.println("I/O error: " + e);
+                }
+                catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+
+
+    }
+
+    private class Controller extends Thread {
+
+        private Socket socket;
+
+        private String in;
+
+        public Controller(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+
+            try {
+                getStreams();
+                output.writeObject("Hello, Welocme to Raspberry PI");
+                output.flush();
+                while (!(in = (String) input.readObject()).equals("close")) {
+                    if (!in.equals("end")) {
+                        String PcIn = in;
+                        switch (in) {
+                            case "10":
+                                System.out.println("Light Off!");
+                                led1.low();
+                                break;
+                            case "11":
+                                System.out.println("Light On!");
+                                led1.high();
+                                break;
+
+                        }
+
+                        new Thread(new Runnable() {
+                            public void run() {
+                                try {
+                                    switch (PcIn) {
+                                        case "20":
+                                            if (x == 0) {
+                                                outputA = new ObjectOutputStream(clients.get("Pc client"));
+                                                outputA.flush();
+                                                outputA.writeObject("20");
+                                                System.out.println("PC Off");
+                                                x = 1;
+                                            }
+                                            else{
+                                                System.out.println("Pc already off!");
+                                            }
+                                            break;
+                                        case "21":
+                                            System.out.println("Wait");
+                                            PC.high();
+                                            Thread.sleep(400);
+                                            PC.low();
+                                            System.out.println("PC On");
+                                            break;
+                                    }
+                                    try {
+                                        output.writeObject("command" + in.toUpperCase());
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                    PC.low();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }).start();
+
+                    } else {
+
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("Error handling client @ " + socket.getRemoteSocketAddress() + ": " + e.getMessage());
+            } catch (ClassNotFoundException ex) {
+                ex.printStackTrace();
+            }  finally {
+                closeConnection();
+                System.out.println("Connection with client @ " + socket.getRemoteSocketAddress() + " closed");
+            }
+        }
+
+        public void getStreams() throws IOException {
+            output = new ObjectOutputStream(socket.getOutputStream());
+            output.flush();
+            //outputB = new ObjectOutputStream(clients.get("Android client"));
+            //outputB.flush();
+            //outputA = new ObjectOutputStream(clients.get("Pc client"));
+            //outputA.flush();
+
+
+        }
+
+        private void closeConnection() {
+            try {
+                output.close();
+                input.close();
+                socket.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+}
+
