2fff9471e747f0e2b7155f9fb58d8e6aea9ae529
Hule-Elev <oulstein@gmail.com>
2017-09-28 22:18:06 +0200

    f

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,9 +1,17 @@
 
-import com.pi4j.io.gpio.GpioController;
-import com.pi4j.io.gpio.GpioFactory;
-import com.pi4j.io.gpio.GpioPinDigitalOutput;
-import com.pi4j.io.gpio.PinState;
-import com.pi4j.io.gpio.RaspiPin;
+//import com.pi4j.io.gpio.GpioController;
+//import com.pi4j.io.gpio.GpioFactory;
+//import com.pi4j.io.gpio.GpioPinDigitalOutput;
+//import com.pi4j.io.gpio.PinState;
+//import com.pi4j.io.gpio.RaspiPin;
+import com.dropbox.core.DbxException;
+import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.v2.DbxClientV2;
+import com.dropbox.core.v2.files.ListFolderResult;
+import com.dropbox.core.v2.files.Metadata;
+import com.dropbox.core.v2.users.FullAccount;
+
+import javax.swing.*;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -22,13 +30,12 @@ public class Server {
     private ObjectOutputStream output;
     private HashMap<String, DataOutputStream> clients;
     private int x = 1;
-    private GpioPinDigitalOutput led1;
-    private GpioPinDigitalOutput PC;
-    final GpioController gpio = GpioFactory.getInstance();
+    //private GpioPinDigitalOutput led1;//private GpioPinDigitalOutput PC;
+    // final GpioController gpio = GpioFactory.getInstance();
 
     public Server() {
-        led1 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, PinState.LOW);
-        PC = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, PinState.LOW);
+       // led1 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, PinState.LOW);
+       // PC = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, PinState.LOW);
     }
 
     public static void main(String[] args) {
@@ -94,11 +101,10 @@ public class Server {
                         switch (in) {
                             case "10":
                                 System.out.println("Light Off!");
-                                led1.high();
-                                break;
+//                                break;
                             case "11":
                                 System.out.println("Light On!");
-                                led1.low();
+                              //  led1.low();
                                 break;
                         }
                         new Thread(new Runnable() {
@@ -118,16 +124,16 @@ public class Server {
                                             break;
                                         case "21":
                                             System.out.println("Wait");
-                                            PC.low();
+                                            //PC.low();
                                             Thread.sleep(400);
-                                            PC.high();
+                                           // PC.high();
                                             System.out.println("PC On");
                                             break;
                                     }
 
                                 } catch (InterruptedException e) {
                                     e.printStackTrace();
-                                    PC.high();
+                                    //PC.high();
                                 }catch (IOException e) {
                                     e.printStackTrace();
                                 }
@@ -177,6 +183,7 @@ public class Server {
     }
     private void closeConnection() {
         try {
+
             //output.close();
             //input.close();
             inputA.close();
@@ -187,3 +194,53 @@ public class Server {
     }
 }
 
+class updater{
+    private static final String ACCESS_TOKEN = "gJKkMgm0o8AAAAAAAAAAYSuQZFy2plTSmSAt02gsXVAxn2gTn-qVBaqIMvdZwy8q";
+
+    public static void main(String [] args) throws DbxException {
+        byte[] b = {65,66,67,68,69};;
+
+        DbxRequestConfig config = new DbxRequestConfig("dropbox", "en_US");
+        DbxClientV2 client = new DbxClientV2(config, ACCESS_TOKEN);
+
+        FullAccount account = client.users().getCurrentAccount();
+        System.out.println(account.getName().getDisplayName());
+
+        ListFolderResult result = client.files().listFolder("");
+        while (true) {
+            for (Metadata metadata : result.getEntries()) {
+                if (metadata.getPathLower().equals("/text.jar")) {
+                    client.files().delete("/text.jar");
+                }
+                try {
+                    InputStream in = new FileInputStream("C:\\Users\\Hule-Elev\\IdeaProjects\\GIt test\\IoT\\target\\IOT_RaspberryPI.jar");
+                    client.files().uploadBuilder("/text.jar").uploadAndFinish(in);
+                    System.out.println("text.jar has been uploaded to dropbox");
+                    //OutputStream downloadFile = new FileOutputStream("C:\\Users\\Hule-Elev\\IdeaProjects\\text.jar");
+                    /*try {
+                        if (metadata.getPathLower().equals("/text.jar")) {
+                            client.files().downloadBuilder("/text.jar").download(downloadFile);
+                            System.out.println("text.txt has been downloaded!");
+                        }
+                    }  finally {
+                        downloadFile.close();
+                    }*/
+                }
+                catch (DbxException e)
+                {
+                    JOptionPane.showMessageDialog(null, "Unable to download file to local system\n Error: " + e);
+                }
+                catch (IOException e)
+                {
+                    JOptionPane.showMessageDialog(null, "Unable to download file to local system\n Error: " + e);
+                }
+            }
+
+            if (!result.getHasMore()) {
+                break;
+            }
+
+            result = client.files().listFolderContinue(result.getCursor());
+        }
+    }
+}
